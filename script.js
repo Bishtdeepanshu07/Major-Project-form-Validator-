a58c3fa9b8fa3834101dc9ca470889cec3e04224
Bishtdeepanshu07 <101801402+Bishtdeepanshu07@users.noreply.github.com>
2024-08-15 17:45:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
+
 function error(input, message) {
     input.className = 'form-control is-invalid';
     const div = input.nextElementSibling;
@@ -11,10 +12,13 @@ function error(input, message) {
     div.className = 'invalid-feedback';
 }
 
+
+//function if input is give in form 
 function success(input) {
     input.className = 'form-control is-valid';
 }
 
+//Function for Email Validation
 function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    
@@ -29,6 +33,7 @@ function checkEmail(input) {
     
 }
 
+//for check if the input is valid
 function checkRequired(inputs) {
     inputs.forEach(function(input) {
         if(input.value === '') {
@@ -39,6 +44,7 @@ function checkRequired(inputs) {
     });  
 }
 
+//function for minimum and maximum length
 function checkLength(input, min, max) {
     if (input.value.length < min) {
         error(input, `${input.id} must be at least ${min} characters`);
@@ -50,12 +56,14 @@ function checkLength(input, min, max) {
      
 }
 
+//function for checking whether the confirm password input is same as password input or not
 function checkPasswords(input1,input2) {
     if(input1.value !== input2.value) {
         error(input2, 'Passwords not match');
     }
 }
 
+//function for phone number validation
 function checkPhone(input) {
     var exp = /^\d{10}$/;   
     if(!exp.test(input.value)) 
